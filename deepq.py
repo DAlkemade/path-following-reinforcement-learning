b4a6230449360d4fec1adf1bc4412e53a7521df3
daan <28051445+DAlkemade@users.noreply.github.com>
2020-03-11 14:33:19 +0000

    Use standard keras model

--- deepq.py
+++ deepq.py
@@ -25,32 +25,6 @@ for v in discrete_vs:
     for w in discrete_ws:
         discrete_actions.append(np.array([v, w]))
 
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(input_shape=(20,)),
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dropout(0.2),
-#     tf.keras.layers.Dense(len(discrete_actions))
-# ])
-class MyModel(tf.keras.Model):
-    def __init__(self, num_states, hidden_units, num_actions):
-        super(MyModel, self).__init__()
-        self.input_layer = tf.keras.layers.InputLayer(input_shape=(num_states,))
-        self.hidden_layers = []
-        for i in hidden_units:
-            self.hidden_layers.append(tf.keras.layers.Dense(
-                i, activation='tanh', kernel_initializer='RandomNormal'))
-        self.output_layer = tf.keras.layers.Dense(
-            num_actions, activation='linear', kernel_initializer='RandomNormal')
-
-    @tf.function
-    def call(self, inputs):
-        z = self.input_layer(inputs)
-        for layer in self.hidden_layers:
-            z = layer(z)
-        output = self.output_layer(z)
-        return output
-
-
 
 class Memory(object):
     """Store robot runs.
@@ -81,7 +55,12 @@ env = gym.make("PathFollower-v0")
 num_states = len(env.observation_space.sample())
 hidden_units = [200, 200]
 num_actions = len(discrete_actions)
-model = MyModel(num_states, hidden_units, num_actions)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(num_states,)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(num_actions)
+])
 
 memory = Memory(MEMORY_SIZE)
 
@@ -128,11 +107,3 @@ for i_episode in range(NUM_RUNS):
             break
 env.close()
 
-# model.evaluate(x_test, y_test, verbose=2)
-#
-# probability_model = tf.keras.Sequential([
-#     model,
-#     tf.keras.layers.Softmax()
-# ])
-#
-# probability_model(x_test[:5])
