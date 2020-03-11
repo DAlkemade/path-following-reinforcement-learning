c76bafbbb808e3255bafb40f711f3a74a31e6b5c
daan <28051445+DAlkemade@users.noreply.github.com>
2020-03-11 14:48:11 +0000

    Extract DQN to class

--- deepq.py
+++ deepq.py
@@ -19,7 +19,7 @@ BATCH = 15
 mnist = tf.keras.datasets.mnist
 
 discrete_vs = np.linspace(0, 1, 3)
-discrete_ws = np.linspace(-np.pi, np.pi, 5) # make sure it's uneven, so that 0. is in there
+discrete_ws = np.linspace(-np.pi, np.pi, 5)  # make sure it's uneven, so that 0. is in there
 discrete_actions = []
 for v in discrete_vs:
     for w in discrete_ws:
@@ -43,7 +43,7 @@ class Memory(object):
     def sample(self, size):
         return random.sample(self.memory, size)
 
-optimizer = tf.optimizers.Adam(0.01)
+
 Experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state', 'done'])
 
 # loss_fn = tf.keras.losses.MeanSquaredError()
@@ -55,14 +55,45 @@ env = gym.make("PathFollower-v0")
 num_states = len(env.observation_space.sample())
 hidden_units = [200, 200]
 num_actions = len(discrete_actions)
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(num_states,)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(num_actions)
-])
 
 memory = Memory(MEMORY_SIZE)
+
+
+class DQN(object):
+    def __init__(self, observation_space_size, action_space_size):
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(observation_space_size,)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(action_space_size)
+        ])
+        self.optimizer = tf.optimizers.Adam(0.01)
+
+    def predict(self, states):
+        return self.model(states)
+
+    # @tf.function
+    def train(self, batch):
+        states, actions, rewards, next_states, dones = zip(*batch)
+        rewards = np.array(rewards)
+        dones = np.array(dones)
+        next_states = np.reshape(next_states, (BATCH, num_states))
+        states = np.reshape(states, (BATCH, num_states))
+        next_predictions = self.model(next_states)
+        next_predictions_max = np.max(next_predictions, axis=1)
+        total_rewards_discounted = rewards + GAMMA * next_predictions_max
+        total_rewards_discounted_include_done = np.where(dones, rewards, total_rewards_discounted)
+
+        with tf.GradientTape() as tape:
+            selected_action_values = tf.math.reduce_sum(self.model(states) * tf.one_hot(actions, len(discrete_actions)),
+                                                        axis=1)
+            loss = tf.math.reduce_sum(tf.square(total_rewards_discounted_include_done - selected_action_values))
+        variables = self.model.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+
+
+train_network = DQN(num_states, num_actions)
 
 for i_episode in range(NUM_RUNS):
     observation = env.reset()
@@ -73,7 +104,7 @@ for i_episode in range(NUM_RUNS):
         if random.random() < EPSILON:
             action_index = random.randint(0, len(discrete_actions) - 1)
         else:
-            prediction = model(np.reshape(observation, (1, num_states)))
+            prediction = train_network.predict(np.reshape(observation, (1, num_states)))
             action_index = np.argmax(prediction)
 
         action = discrete_actions[action_index]
@@ -82,28 +113,11 @@ for i_episode in range(NUM_RUNS):
         memory.append(Experience(prev_observation, action_index, reward, observation, done))
         if t % BATCH == 0:
             # TODO if choosing to use a larger batch size, do this in a @tf.function
-            batch = memory.sample(BATCH)
-            states, actions, rewards, next_states, dones = zip(*batch)
-            rewards = np.array(rewards)
-            dones = np.array(dones)
-            next_states = np.reshape(next_states, (BATCH, num_states))
-            states = np.reshape(states, (BATCH, num_states))
-            next_predictions = model(next_states)
-            next_predictions_max = np.max(next_predictions, axis=1)
-            total_rewards_discounted = rewards + GAMMA * next_predictions_max
-            total_rewards_discounted_include_done = np.where(dones, rewards, total_rewards_discounted)
-
-            with tf.GradientTape() as tape:
-                selected_action_values = tf.math.reduce_sum(model(states) * tf.one_hot(actions, len(discrete_actions)), axis=1)
-                loss = tf.math.reduce_sum(tf.square(total_rewards_discounted_include_done - selected_action_values))
-            variables = model.trainable_variables
-            gradients = tape.gradient(loss, variables)
-            optimizer.apply_gradients(zip(gradients, variables))
-
+            batch_train = memory.sample(BATCH)
+            train_network.train(batch_train)
             # model.fit(states, total_rewards_discounted_include_done, epochs=1)
 
         if done:
             print("Episode finished after {} timesteps".format(t + 1))
             break
 env.close()
-
