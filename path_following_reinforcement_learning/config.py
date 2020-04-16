ce2da5e86286081dee15d006a2d2086db131932e
daan <28051445+DAlkemade@users.noreply.github.com>
2020-04-16 09:32:45 +0200

    increase memory

--- path_following_reinforcement_learning/config.py
+++ path_following_reinforcement_learning/config.py
@@ -5,6 +5,6 @@ class Config:
         self.batch_size = 1000
         self.copy_step = 25
         self.max_steps_in_run = 1000
-        self.memory_size = 1000000
+        self.memory_size = 100000
         self.num_layers = 2
         self.learning_rate = .01
